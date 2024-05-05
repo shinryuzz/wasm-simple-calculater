734b957aa1b10d650e6b8845fade76cf4167e98e
Shinryuzz <mshin1127@gmail.com>
2024-05-05 14:04:45 +0900

    feat: add alert function by wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import init, { greet } from "../wasm/pkg/wasm.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    init();
+  });
 
   return (
     <>
@@ -18,9 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => greet("World!")}>Greet</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
